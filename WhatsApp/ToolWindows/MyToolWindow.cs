8adb23efb76e6772a72d65a08b39408c3686b4ed
Himanshu kumar Sharma <SharmaHi@incadeadev.com>
2024-12-25 15:24:21 +0530

    Added whatsapp code

---
+++ WhatsApp/ToolWindows/MyToolWindow.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WhatsApp
+{
+    public class MyToolWindow : BaseToolWindow<MyToolWindow>
+    {
+        public override string GetTitle(int toolWindowId) => "WhatsAPP";
+
+        public override Type PaneType => typeof(Pane);
+
+        public override Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<FrameworkElement>(new MyToolWindowControl());
+        }
+
+        [Guid("f41967c3-84b6-4cce-8f0e-3d017b2763d0")]
+        internal class Pane : ToolkitToolWindowPane
+        {
+            public Pane()
+            {
+                BitmapImageMoniker = KnownMonikers.ToolWindow;
+            }
+        }
+    }
+}
---
+++ WhatsApp/ToolWindows/MyToolWindowControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WhatsApp
+{
+    public partial class MyToolWindowControl : UserControl
+    {
+        public MyToolWindowControl()
+        {
+            InitializeComponent();
+            InitializeAsync();
+        }
+        private async void InitializeAsync()
+        {
+            Random rnd = new Random();
+            string subFolder = rnd.Next().ToString();
+            var env = await Microsoft.Web.WebView2.Core.CoreWebView2Environment.CreateAsync(userDataFolder: Path.Combine(Path.GetTempPath(), $"{Environment.UserName}", subFolder));
+            await WhatsApp.EnsureCoreWebView2Async(env);
+            var request = WhatsApp.CoreWebView2.Environment.CreateWebResourceRequest("https://web.whatsapp.com/", "GET", null, null);
+            WhatsApp.CoreWebView2.NavigateWithWebResourceRequest(request);
+        }
+    }
+}
